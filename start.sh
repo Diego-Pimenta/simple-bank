c2c246aedfc6acfc5ad1ff09a0831f59ff55f4e1
Diego Pimenta <diegopimenta888@gmail.com>
2025-05-22 21:56:02 -0300

    fix: api container not running locally

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "run db migration"
-source /app/app.env
+# required to update variables when loading from secrets manager
+# source /app/app.env
 /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
